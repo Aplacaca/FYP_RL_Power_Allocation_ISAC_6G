f2d025436346b6fd7d8bb24220a63f02a2eb522b
dhy <3057931787@qq.com>
2023-04-13 01:31:51 +0800

    chnage param

--- env/env.py
+++ env/env.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from utils import f_Theta
 
 import pdb
-class BS1(object):
+class BS_TD(object):
     def __init__(self,K=5):
         self.K = K
         self.a1 = np.random.rand(K).astype(np.float128)*10000
@@ -85,26 +85,38 @@ class BS1(object):
     def radar_estimation_rate(self,):
         duty_factor = 0.1 # Pulse Duration Interval / Pulse Repetition Interval
         T = 0.000001 # 1us
-        sigma = 0.01
+        sigma = 0.01 # Processing Noise
         R_est = duty_factor/(2*T) * np.log2(1 + 4*(np.pi**2)*(sigma**2)*(self.B**2)*T*self.g*self.P_s/self.delta)
         return R_est
     
     def update_channel(self,):
         self.g = np.random.rand(self.K).astype(np.float128)*0.0001
-        
+
+
 class UE(object):
-    def __init__(self, id, N_r=1, N_t=4, is_s=0, is_c=0):
+    def __init__(self, id, N_r=1, N_t=16, is_s=0, is_c=0):
         self.id = id
         self.N_r = N_r
         self.N_t = N_t
-        self.h = np.random.rand(self.N_r,self.N_t).astype(np.float64)*0.0001 #
-        u_,si,v_h = np.linalg.svd(self.h) # receiver
+        # 
+        self.d = np.random.rand()*2+1.0 # m
+        self.theta = np.random.rand()*np.pi# angle 0~pi
+        self.g = (3*10e8/(4*np.pi*self.d*10*10e9))**2*1000# PathLoss
+        self.at = np.expand_dims(np.exp(1j * np.arange(start=0, stop=self.N_t , step=1, dtype=np.complex128) * np.pi *np.sin(self.theta)), axis=-1)
+        self.ar = np.expand_dims(np.exp(1j * np.arange(start=0, stop=self.N_r , step=1, dtype=np.complex128) * np.pi *np.sin(self.theta)), axis=-1)
+        #
+        self.h = self.ar@self.at.conjugate().transpose()    #
+        # self.h = np.random.rand(self.N_r,self.N_t).astype(np.float64)    #
+        self.h_r = self.at@self.at.conjugate().transpose()
+        u_,si,v_h = np.linalg.svd(self.h_r) # receiver
         u_h = u_.conjugate().transpose()
         v = np.expand_dims(v_h[0,:], axis=0).conjugate().transpose()
+        # pdb.set_trace()
         self.h_norm = np.linalg.norm(self.h)
         self.u = u_h
-        self.v = v
-        self.P_n = np.random.rand()*0.0001 # noise power
+        self.v = deepcopy(self.at)
+        #
+        self.P_n = 1e-8 # noise power
         self.P_c = 0.0 # Downlink Communication power
         self.P_s = 0.0 # Downlink Sensing power
         self.is_s = is_s 
@@ -115,26 +127,45 @@ class UE(object):
         self.Sinr = 0.0 # db
         # NOMA Rate
         self.R_c = 0.0 # bps
+        print("range:", self.d)
+        print("g:", self.g)
 
     def update(self,):
-        self.h = np.random.rand(self.N_r,self.N_t).astype(np.float64)*0.0001 # 
-        u_,si,v_h = np.linalg.svd(self.h) # receiver
+        # 
+        self.d = np.random.rand()*5 # m
+        self.theta = np.random.rand()*np.pi# angle 0~pi
+        self.g = (3*10e8/(4*np.pi*self.d*10*10e9))**2# PathLoss
+        self.at = np.expand_dims(np.exp(1j * np.arange(start=0, stop=self.N_t , step=1, dtype=np.complex128) * np.pi *np.sin(self.theta)), axis=-1)
+        self.ar = np.expand_dims(np.exp(1j * np.arange(start=0, stop=self.N_r , step=1, dtype=np.complex128) * np.pi *np.sin(self.theta)), axis=-1)
+        #
+        # pdb.set_trace()
+        self.h = self.ar@self.at.conjugate().transpose()    #
+        # self.h = np.random.rand(self.N_r,self.N_t).astype(np.float64)    #
+        self.h_r = self.at@self.at.conjugate().transpose()
+        u_,si,v_h = np.linalg.svd(self.h_r) # receiver
         u_h = u_.conjugate().transpose()
         v = np.expand_dims(v_h[0,:], axis=0).conjugate().transpose()
-        self.u = u_h
-        self.v = v
         self.h_norm = np.linalg.norm(self.h)
-
+        self.u = u_h
+        # self.v = v
+        self.v = deepcopy(self.at)
+        
         
 
 class BS(object):
-    def __init__(self, N_t=4, N_c=5, N_s=2):
+    def __init__(self, N_t=16, N_c=5, N_s=2):
         self.c_UE = [UE(id="c"+str(i),N_t=N_t,is_c=1) for i in range(N_c)]  
         # Sort to H norm descending     
-        self.c_UE.sort(key=lambda x : x.h_norm, reverse = True)
+        self.c_UE.sort(key=lambda x : x.g*x.h_norm, reverse = True)
         self.s_UE = [UE(id="s"+str(i),N_t=N_t,is_s=1) for i in range(N_s)] 
         self.B_c = 10e+6 # Hz Communication Bandwidth
         self.B_s = 10e+6 # Hz Sensing Bandwidth
+        self.P_tot = 500 # W
+        self.P_n = 1e-5 # noise power
+        self.N_t = N_t
+        self.N_c = N_c
+        self.N_s = N_s
+        
         # pdb.set_trace()
 
     def _cal_noma(self, update = True):
@@ -143,34 +174,81 @@ class BS(object):
         for _id in range(len(self.c_UE)):
             UE = self.c_UE[_id]
             UE_in = self.c_UE[0:_id]
-            P_si = np.linalg.norm(UE.H)*UE.P_c
+            P_si = UE.g*UE.h_norm*UE.P_c
             P_in = 0.0
             if len(UE_in) > 0:
-                P_in = np.linalg.norm(UE.H)*sum([UE_.P_c for UE_ in UE_in])
-            Sinr.append(P_si/(P_in+UE.P_n))
-            R_c.append(UE.B_c * np.log2(1+UE.Sinr))
+                P_in = UE.g*UE.h_norm*sum([UE_.P_c for UE_ in UE_in])
+            Sinr.append((P_si/(P_in+UE.P_n)))
+            R_c.append(self.B_c * np.log2(1+P_si/(P_in+UE.P_n)))
             if update:
                 UE.Sinr = P_si/(P_in+UE.P_n)
-                UE.R_c = UE.B_c * np.log2(1+UE.Sinr)   
+                UE.R_c = self.B_c * np.log2(1+UE.Sinr)   
         return Sinr,R_c  
 
     def _cal_radar(self, update=True):   
-        ...
+        R_est = []
+        # Radar Processing Params 
+        duty_factor = 0.1 # Pulse Duration Interval / Pulse Repetition Interval
+        T = 0.000001 # 1us
+        sigma = 0.01 # Processing Noise
+        SUM_COMA = np.identity(self.N_t,dtype=np.complex128)*self.P_n
+        for _id in range(len(self.s_UE)):
+            UE = self.s_UE[_id]
+            SUM_COMA += UE.g**4*UE.h_r@UE.v@(UE.v.transpose().conjugate())@(UE.h_r.transpose().conjugate())
+            # pdb.set_trace()
+        for _id in range(len(self.s_UE)):
+            UE = self.s_UE[_id]
+            UE_ = self.s_UE[0:_id]+self.s_UE[_id+1:]
+            P_in = 0 # Total inference signal power (without noise)
+            COMA = SUM_COMA - UE.g**4*UE.h_r@UE.v@(UE.v.transpose().conjugate())@(UE.h_r.transpose().conjugate())
+            UE.u = np.linalg.inv(COMA)@UE.h_r@UE.v@np.linalg.inv(UE.v.transpose().conjugate()@UE.h_r.transpose().conjugate()@np.linalg.inv(COMA)@UE.h_r@UE.v)
+            UE.u = UE.u.transpose().conjugate()
+            # pdb.set_trace()
+            for UE_in in UE_:
+                P_in += (UE_in.g)**2*np.linalg.norm(UE.u@UE_in.h_r@UE.v)*UE.P_s
+            P_useful = (UE.g)**2*np.linalg.norm(UE.u@UE.h_r@UE.v)*UE.P_s
+            R_ = duty_factor/(2*T) * np.log2(1 + 4*(np.pi**2)*(sigma**2)*(self.B_s**2)*T*P_useful/(self.P_n*np.linalg.norm(UE.u)+P_in))
+            R_est.append(R_)
+            print("RSINR(dB):",10*np.log(P_useful/(self.P_n*np.linalg.norm(UE.u)+P_in)) )
+            print("Pin: ",P_in)
+            print("P_useful: ",P_useful)
+            # pdb.set_trace()
+        return R_est
         
-    def update(self,):
+    def update_channels(self,):
         # Update Communication UE Channel
         for UE in self.c_UE:
             UE.update()
-        # Resort NOMA Index
-        self.c_UE.sort(key=lambda x : x.h_norm, reverse = True)
+        # Re-sort NOMA Index
+        self.c_UE.sort(key=lambda x : x.g*x.h_norm, reverse = True)
         # Update Sensing UE Channel
         for UE in self.s_UE:
             UE.update()
+
+    def get_performance(self,):
+        Sinr, R_c = self._cal_noma()
+        R_est = self._cal_radar()
+        print("Sinr (dB)", 10*np.log(Sinr))
+        print("R_c", R_c)
+        print("R_est", R_est)
+        return R_c, R_est
+
+    def _power_allocation(self, Pc_s, Ps_s):
+        for _id in range(len(self.s_UE)):
+            UE = self.s_UE[_id]
+            UE.P_s = Ps_s[_id]
+        for _id in range(len(self.c_UE)):
+            UE = self.c_UE[_id]
+            UE.P_c = Pc_s[_id]
+        
+    
         
 
 if __name__ == "__main__":
     np.random.seed(777)
-    env = BS()
+    env = BS(N_t=16)
+    env._power_allocation(Pc_s = [10,20,50,90,150], Ps_s = [1,1])
+    env.get_performance()
     # env.TD_allocate()
     # print("a1:",env.a1)
     # print("a2:",env.a2)
